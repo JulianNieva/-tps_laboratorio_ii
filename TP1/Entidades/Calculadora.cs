c55fa4a399100bd9febdb25b2882ce3c6e71c64e
JulianNieva <juliannieva90@gmail.com>
2022-04-04 21:55:40 -0300

    Creado proyecto entidades y agregado clase calculadora

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        /// <summary>
+        /// Realiza la operacion matematica correspondiente
+        /// </summary>
+        /// <param name="num1">Primer operando</param>
+        /// <param name="num2">Segundo operando</param>
+        /// <param name="operador">Operacion a realizar</param>
+        /// <returns>Retorna el resultado de la operacion realziada</returns>
+        public static double Operar(Operando num1, Operando num2, char operador)
+        {
+            double resultado = 0;
+
+            switch (Calculadora.ValidarOperador(operador))
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    resultado = num1 / num2;
+                    break;
+
+            }
+
+            return resultado;
+        }
+
+        /// <summary>
+        /// Valida el operador recibido
+        /// </summary>
+        /// <param name="operador">Operador a validar</param>
+        /// <returns>Retorna el operador que corresponde, si es invalido, retorna +</returns>
+        private static char ValidarOperador(char operador)
+        {
+            char retorno = '+';
+
+            if(operador == '-' || operador == '*' || operador == '/')
+            {
+                retorno = operador;
+            }
+
+            return retorno;
+        }
+
+    }
+}
