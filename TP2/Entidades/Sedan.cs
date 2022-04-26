ce42eda1f6c5336fc16a8fdcbca2e56d9d24a476
JulianNieva <juliannieva90@gmail.com>
2022-04-26 20:02:09 -0300

    Arreglado clase Sedan

--- TP2/Entidades/Sedan.cs
+++ TP2/Entidades/Sedan.cs
@@ -19,20 +19,24 @@ namespace Entidades
         /// <param name="marca"></param>
         /// <param name="chasis"></param>
         /// <param name="color"></param>
-        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+        public Sedan(EMarca marca, string chasis, ConsoleColor color):this(marca,chasis,color,ETipo.CuatroPuertas)
+        {
+        }
+
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
             : base(chasis, marca, color)
         {
-            tipo = ETipo.CuatroPuertas;
+            this.tipo = tipo;
         }
 
         /// <summary>
         /// Sedan son 'Mediano'
         /// </summary>
-        protected override short Tamanio
+        protected override ETamanio Tamanio
         {
             get
             {
-                return this.Tamanio;
+                return ETamanio.Mediano;
             }
         }
 
@@ -41,13 +45,12 @@ namespace Entidades
             StringBuilder sb = new StringBuilder();
 
             sb.AppendLine("SEDAN");
-            sb.AppendLine(this);
-            sb.AppendLine("TAMAÑO : {0}", this.Tamanio);
+            sb.Append(base.Mostrar());
             sb.AppendLine("TIPO : " + this.tipo);
             sb.AppendLine("");
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
     }
 }
